e5c18e83c7a61f7ededd2e2570b273acd9367e0f
Shiva-0707 <shivanshsri0005@gmail.com>
2024-12-22 22:16:32 +0530

    first commit

---
+++ src/main/java/com/docker/jenkin_integration/JenkinIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.docker.jenkin_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docker/jenkin_integration/JenkinIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.jenkin_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
